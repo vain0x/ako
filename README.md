e6a192c9d18875e44262b1a5c906d7f1b2e14031
vain0x <vainzerox@gmail.com>
2021-02-02 00:34:51 +0900

    feat: Add notepad

---
+++ README.md
@@ -0,0 +1,3 @@
+# Army Knife Online
+
+- [notepad](https://vain0x/github.io/army-knife-online/notepad/)
---
+++ docs/notepad/notepad.js
@@ -0,0 +1,50 @@
+const TITLE = "Notepad"
+
+const main = () => {
+  const textAreaElement = document.getElementById("textarea")
+
+  const load = () => {
+    let text = ""
+    try {
+      text = window.localStorage.getItem("text") ?? ""
+    } catch {
+      return
+    }
+
+    if (text !== "") {
+      textAreaElement.value = text
+    }
+  }
+
+  const save = () => {
+    const text = textAreaElement.value
+    if (text.length === 0) {
+      window.localStorage.removeItem("text")
+    } else {
+      window.localStorage.setItem("text", text)
+    }
+  }
+
+  let dirty = false
+
+  const triggerAutoSave = () => {
+    if (!dirty) {
+      document.title = `${TITLE}*`
+      dirty = true
+
+      setTimeout(() => {
+        if (dirty) {
+          save()
+          document.title = TITLE
+          dirty = false
+        }
+      }, 300)
+    }
+  }
+
+  textAreaElement.addEventListener("input", triggerAutoSave)
+
+  load()
+}
+
+document.addEventListener("DOMContentLoaded", main)

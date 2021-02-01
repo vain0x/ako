968ee0d847f30bdac42443bc1f8f3203e8fd330f
vain0x <vainzerox@gmail.com>
2021-02-02 01:19:16 +0900

    feat: Add cheap canvas

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Army Knife Online
 
-- [notepad](https://vain0x/github.io/army-knife-online/notepad/)
+- [canvas](https://vain0x.github.io/army-knife-online/canvas/)
+- [notepad](https://vain0x.github.io/army-knife-online/notepad/)
---
+++ docs/canvas/canvas.js
@@ -0,0 +1,45 @@
+const TAU = Math.PI * 2
+
+const main = () => {
+  const canvasElement = document.getElementById("canvas")
+
+  const onResize = ()=> {
+    canvasElement.width = document.body.clientWidth
+    canvasElement.height = document.body.clientHeight
+  }
+
+  onResize()
+  // document.addEventListener("resize", onResize)
+
+  canvasElement.addEventListener("mousedown", ev => {
+    if (!(ev.button === 0 && !ev.altKey && !ev.ctrlKey && !ev.shiftKey && !ev.metaKey)) {
+      return
+    }
+
+    const onMouseMove = ev => {
+      const { x, y } = ev
+
+      window.requestAnimationFrame(() => {
+        const context = canvasElement.getContext("2d")
+        context.fillStyle = "#212121"
+        context.beginPath()
+        context.ellipse(x, y, 3, 3, 0, 0, TAU)
+        context.fill()
+      })
+    }
+
+    canvasElement.addEventListener("mousemove", onMouseMove)
+
+    canvasElement.addEventListener("mouseup", () => {
+      canvasElement.removeEventListener("mousemove", onMouseMove)
+    }, { once: true })
+
+    canvasElement.addEventListener("mouseleave", () => {
+      canvasElement.removeEventListener("mousemove", onMouseMove)
+    }, { once: true })
+
+    onMouseMove(ev)
+  })
+}
+
+document.addEventListener("DOMContentLoaded", main)

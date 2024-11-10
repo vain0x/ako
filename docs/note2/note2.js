632ee835f885a7183e58f9396108512a14d6e711
vain0x <vainzerox@gmail.com>
2024-11-10 13:15:46 +0900

    feat: Update note2

--- docs/note2/note2.js
+++ docs/note2/note2.js
@@ -1,6 +1,4 @@
-const TITLE = "note2"
-const events = new EventTarget()
-
+const TITLE = document.title
 const DRY = false
 
 // -----------------------------------------------
@@ -13,102 +11,58 @@ const textToBase64 = s => bytesToBase64(textEncoder.encode(s))
 const textFromBase64 = s => textDecoder.decode(base64ToBytes(s))
 // https://developer.mozilla.org/ja/docs/Glossary/Base64
 function base64ToBytes(base64) {
-  const binString = atob(base64);
+  const binString = window.atob(base64)
   return Uint8Array.from(binString, (m) => m.codePointAt(0))
 }
 function bytesToBase64(bytes) {
-  return btoa(Array.from(bytes, byte => String.fromCodePoint(byte)).join(""))
+  return window.btoa(Array.from(bytes, byte => String.fromCodePoint(byte)).join(""))
 }
 
 // -----------------------------------------------
-// settings
+// app
 // -----------------------------------------------
 
 const storage = window.localStorage
 
-const loadSettings = () => {
-  return {
-    autosave: storage.getItem("note2.autosave") === "1",
-    accessToken: storage.getItem("gh.accessToken") || "",
-    owner: storage.getItem("gh.owner") || "",
-    repo: storage.getItem("gh.repo") || "",
-    path: storage.getItem("gh.path") || "",
-    branch: storage.getItem("gh.branch") || "",
-  }
-}
+let lastSha = storage.getItem("note2.lastSha") || null
+let lastContents = storage.getItem("note2.lastContents") || null
+let isSaved = true
 
-let settings = loadSettings()
-let isSettingsDirty = false
-
-// -----------------------------------------------
-// control dialog
-// -----------------------------------------------
-
-const controlDialogEl = document.getElementById("control-dialog")
-
-const computeSettingsSummary = () => {
-  return `github.com@${settings.accessToken ? "**" : ""}:${settings.owner || "_"}/${settings.repo || "_"}/${settings.path || "_"}?ref=${settings.branch || "_"}`
-}
-
-const setSettingsToForm = () => {
-  const autosaveEl = document.getElementsByName("autosave")[0]
-  autosaveEl.checked = settings.autosave
-
-  for (const name of [
-    "accessToken",
-    "owner",
-    "repo",
-    "path",
-    "branch",
-  ]) {
-    const el = document.getElementsByName(name)[0]
-    el.value = settings[name]
-  }
-}
-
-const saveSettings = () => {
-  const get = name => {
-    return document.getElementsByName(name)[0].value.trimEnd() || ""
-  }
-  const set = (key, value) => {
-    if (value) {
-      storage.setItem(key, value)
-    } else {
-      storage.removeItem(key)
-    }
-  }
-  const obj = {
-    autosave: document.getElementsByName("autosave")[0].checked,
-    accessToken: get("accessToken"),
-    owner: get("owner"),
-    repo: get("repo"),
-    path: get("path"),
-    branch: get("branch"),
-  }
-
-  set("note2.autosave", obj.autosave ? "1" : "")
-  for (const name of [
-    "accessToken",
-    "owner",
-    "repo",
-    "path",
-    "branch",
-  ]) {
-    set(`gh.${name}`, obj[name])
-  }
-  settings = obj
-  isSettingsDirty = true
-  events.dispatchEvent(new Event("settingsChanged"))
+let settings = {
+  autosave: storage.getItem("note2.autosave") === "1",
+  accessToken: storage.getItem("gh.accessToken") || "",
+  owner: storage.getItem("gh.owner") || "",
+  repo: storage.getItem("gh.repo") || "",
+  path: storage.getItem("gh.path") || "",
+  branch: storage.getItem("gh.branch") || "",
 }
 
 // -----------------------------------------------
 // main
 // -----------------------------------------------
 
-let lastSha
-let lastContents
+window.addEventListener("error", ev => {
+  showStatus(`ERROR(window): ${ev.error}`)
+  console.error("ERROR(window@error)", ev.error)
+})
 
-const textEl = document.getElementById("textarea")
+window.addEventListener("unhandledrejection", ev => {
+  showStatus(`ERROR(unhandled): ${ev.reason}`)
+  console.error("ERROR(unhandledrejection)", ev.reason)
+})
+
+const markAsSaved = () => {
+  if (!isSaved) {
+    isSaved = true
+    document.title = TITLE
+    storage.removeItem("note2.unsavedContents")
+  }
+}
+const markAsUnsaved = () => {
+  isSaved = false
+  document.title = `${TITLE}*`
+  storage.setItem("note2.unsavedContents", textEl.value)
+}
 
 const fetchContents = async () => {
   if (DRY) return await new Promise(resolve => setTimeout(() => resolve("fetched contents: " + ((new Date()).toLocaleString())), 1000))
@@ -145,12 +99,15 @@ const fetchContents = async () => {
   {
     const data = await res.json()
     const { type, encoding, size, path, content, sha } = data
-    lastSha = sha
     console.log("fetched", type, encoding, size, path, sha)
 
     const decoded = textFromBase64(content)
     console.log("  decoded", decoded)
+
+    lastSha = sha
     lastContents = decoded
+    storage.setItem("note2.lastSha", lastSha)
+    storage.setItem("note2.lastContents", lastContents)
     return decoded
   }
 }
@@ -207,27 +164,37 @@ const fetchUpload = async value => {
 
   {
     const data = await res.json()
-    const { sha } = data
+    const { sha } = data.content
+    console.log("fetched", sha)
+
     lastSha = sha
     lastContents = value
-    console.log("fetched", sha)
+    storage.setItem("note2.lastSha", lastSha)
+    storage.setItem("note2.lastContents", lastContents)
     return true
   }
 }
 
+const loadButtonEl = document.getElementById("load-button")
+loadButtonEl.addEventListener("click", () => {
+  performLoad()
+})
+
 const performLoad = () => {
   showStatus("loading...")
   !(async () => {
-    try {
-      const contents = await fetchContents()
-      textEl.value = contents
-      textEl.focus()
-    } catch (err) {
-      showStatus("ERROR")
-      throw err
-    }
+    const contents = await fetchContents()
+    textEl.value = contents
+    textEl.focus()
+    showStatus("loaded")
+    markAsSaved()
   })()
 }
+
+const saveButtonEl = document.getElementById("save-button")
+saveButtonEl.addEventListener("click", () => {
+  performSave()
+})
 
 const performSave = () => {
   const contents = textEl.value.trimEnd()
@@ -239,52 +206,10 @@ const performSave = () => {
 
   showStatus("saving...")
   !(async () => {
-    try {
-      await fetchUpload(contents)
-      showStatus("saved")
-    } catch (err) {
-      showStatus("ERROR")
-      throw err
-    }
+    await fetchUpload(contents)
+    showStatus("saved")
+    markAsSaved()
   })()
-}
-
-let currentAutosave = null
-
-const handleInputWhenAutoSave = () => {
-  let titleOrig
-  if (currentAutosave) {
-    console.log("autosave recreate")
-    titleOrig = currentAutosave.titleOrig
-    currentAutosave.dispose()
-  } else {
-    titleOrig = document.title
-    document.title = `${titleOrig}*`
-  console.log("autosave start")
-  }
-
-  const flush = () => {
-    console.log("autosave flush")
-    document.title = titleOrig
-    currentAutosave = null
-    dispose()
-    performSave()
-  }
-
-  const dispose = () => {
-    clearTimeout(timeout)
-    document.removeEventListener("visibilitychange", handleVisibilityChange)
-  }
-  const timeout = setTimeout(flush, 3 * 1000)
-
-  const handleVisibilityChange = () => {
-    if (document.hidden) {
-      flush()
-    }
-  }
-  document.addEventListener("visibilitychange", handleVisibilityChange)
-
-  currentAutosave = { titleOrig, dispose }
 }
 
 let statusTimeout
@@ -298,57 +223,124 @@ const showStatus = message => {
   }, 2000)
 }
 
-// init
-{
-  const loadButtonEl = document.getElementById("load-button")
-  loadButtonEl.addEventListener("click", () => {
-    performLoad()
-  })
+let currentAutosave
+const textEl = document.getElementById("textarea")
+textEl.value = lastContents || ""
+textEl.addEventListener("input", () => {
+  markAsUnsaved()
 
-  const saveButtonEl = document.getElementById("save-button")
-  saveButtonEl.addEventListener("click", () => {
-    performSave()
-  })
+  // re-trigger autosave
+  currentAutosave?.dispose()
+  currentAutosave = null
 
-  document.getElementById("control-button").addEventListener("click", () => {
-    setSettingsToForm()
-    controlDialogEl.showModal()
-  })
+  if (settings.autosave) {
+    const flush = () => {
+      console.log("autosave flush")
+      currentAutosave = null
+      dispose()
+      performSave()
+    }
 
-  document.getElementById("control-close-button").addEventListener("click", () => {
-    controlDialogEl.close()
-  })
+    const dispose = () => {
+      clearTimeout(timeout)
+      document.removeEventListener("visibilitychange", handleVisibilityChange)
+    }
+    const timeout = setTimeout(flush, 1000)
 
-  const settingsFormEl = document.getElementById("settings-form")
-  settingsFormEl.addEventListener("submit", ev => {
-    ev.preventDefault()
-    saveSettings()
-    controlDialogEl.close()
-    update()
-  })
-
-  let init = true
-
-  const update = () => {
-    if (init || isSettingsDirty) {
-      loadButtonEl.hidden = settings.autosave
-      saveButtonEl.hidden = settings.autosave
-
-      showStatus(computeSettingsSummary(settings))
-
-      if (settings.autosave) {
-        textEl.addEventListener("input", handleInputWhenAutoSave)
-      } else {
-        textEl.removeEventListener("input", handleInputWhenAutoSave)
+    const handleVisibilityChange = () => {
+      if (document.hidden) {
+        flush()
       }
     }
-    if (!isSettingsDirty) {
-      currentAutosave?.dispose()
-    }
+    document.addEventListener("visibilitychange", handleVisibilityChange)
 
-    init = false
-    isSettingsDirty = false
+    currentAutosave = { dispose }
+  }
+})
+
+const controlDialogEl = document.getElementById("control-dialog")
+
+const computeSettingsSummary = () => {
+  return `github.com@${settings.accessToken ? "**" : ""}:${settings.owner || "_"}/${settings.repo || "_"}/${settings.path || "_"}?ref=${settings.branch || "_"}`
+}
+
+document.getElementById("control-button").addEventListener("click", () => {
+  const autosaveEl = document.getElementsByName("autosave")[0]
+  autosaveEl.checked = settings.autosave
+
+  for (const name of [
+    "accessToken",
+    "owner",
+    "repo",
+    "path",
+    "branch",
+  ]) {
+    const el = document.getElementsByName(name)[0]
+    el.value = settings[name]
   }
 
-  update()
+  controlDialogEl.showModal()
+})
+
+const saveSettings = () => {
+  const get = name => {
+    return document.getElementsByName(name)[0].value.trimEnd() || ""
+  }
+  const set = (key, value) => {
+    if (value) {
+      storage.setItem(key, value)
+    } else {
+      storage.removeItem(key)
+    }
+  }
+  const obj = {
+    autosave: document.getElementsByName("autosave")[0].checked,
+    accessToken: get("accessToken"),
+    owner: get("owner"),
+    repo: get("repo"),
+    path: get("path"),
+    branch: get("branch"),
+  }
+
+  set("note2.autosave", obj.autosave ? "1" : "")
+  for (const name of [
+    "accessToken",
+    "owner",
+    "repo",
+    "path",
+    "branch",
+  ]) {
+    set(`gh.${name}`, obj[name])
+  }
+  settings = obj
+
+  if (!settings.autosave) {
+    currentAutosave?.dispose()
+    currentAutosave = null
+  }
 }
+
+document.getElementById("control-close-button").addEventListener("click", () => {
+  controlDialogEl.close()
+})
+
+const settingsFormEl = document.getElementById("settings-form")
+settingsFormEl.addEventListener("submit", ev => {
+  ev.preventDefault()
+  controlDialogEl.close()
+  saveSettings()
+  handleInitOrSettingsChanged()
+})
+
+const handleInitOrSettingsChanged = () => {
+  loadButtonEl.hidden = settings.autosave
+  saveButtonEl.hidden = settings.autosave
+
+  showStatus(computeSettingsSummary(settings))
+
+  if (settings.autosave && !lastSha) {
+    performLoad()
+  }
+}
+
+handleInitOrSettingsChanged()
